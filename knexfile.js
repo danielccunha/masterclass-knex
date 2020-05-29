3593a081f9170ac3d718487c276144dcc808aacd
Daniel Cunha <danielcunha54@gmail.com>
2020-05-29 20:17:45 -0300

    Setup knex and env variables

---
+++ knexfile.js
@@ -0,0 +1,12 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+    },
+  },
+};
